1e863ef4ed1a44fb181390fe70ab863065afe4dd
giovanna.urrego <giovanna.urrego>
2016-03-17 12:33:19 -0300

    First commit

---
+++ src/main/java/giovanna_urrego/training/globant/com/pages/HomePage.java
@@ -0,0 +1,40 @@
+package giovanna_urrego.training.globant.com.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class HomePage {
+
+  @FindBy(className="entry-title")
+  private WebElement entryTitle;
+  
+  @FindBy(id="s")
+  private WebElement search;
+  
+  @FindBy(id="searchsubmit")
+  private WebElement searchSubmit;
+  
+  
+
+  public void go(WebDriver driver) {
+    driver.get("http://10.28.148.127/wordpress/");
+  }
+
+  public String getPageTitle(WebDriver driver) {
+    return driver.getTitle();
+  }
+  
+  public String search(WebDriver driver, String searchText){
+    search.sendKeys(searchText);
+    searchSubmit.submit();
+    WebDriverWait wait = new WebDriverWait(driver, 10000);
+    String text = (wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("entry-title")))).getText();
+    System.out.println("Imprimo"+text);
+    return text;
+  }
+
+}
---
+++ src/main/java/giovanna_urrego/training/globant/com/tests/Tests.java
@@ -0,0 +1,44 @@
+package giovanna_urrego.training.globant.com.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import giovanna_urrego.training.globant.com.pages.HomePage;
+import junit.framework.Assert;
+
+public class Tests {
+
+  WebDriver driver;
+
+  @BeforeMethod
+  public void before() {
+    driver = new FirefoxDriver();
+  }
+
+/*  @AfterMethod
+  public void after() {
+    driver.quit();
+  }*/
+
+/*  @Test
+  public void verifyTitle() {
+    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+    homePage.go(driver);
+    Assert.assertEquals("Automation Training | Aprender a automatizar en un solo sitio", homePage.getPageTitle(driver));
+  }*/
+  
+  @Test
+  public void verifySearch(){
+    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+    homePage.go(driver);
+    Assert.assertEquals("Nothing Found", homePage.search(driver, "testing"));
+  }
+  
+  //@Test
+  
+
+}
